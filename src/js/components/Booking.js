1826c29021bb057aa4dd5170f8d2eca24584ba19
Bartek K <kontener00@gmail.com>
2020-08-05 00:36:07 +0200

    update with DatePicker, HourPicker .js files

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -11,7 +11,7 @@ export class Booking {
   }
 }
 
-render(element){
+render(element) {
   const thisBooking = this;
   const generatedHTML = templates.bookingWidget();
   thisBooking.dom = {};
@@ -19,4 +19,13 @@ render(element){
   thisBooking.dom.wrapper.innerHTML = generatedHTML;
   thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
   thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
-};
+  thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
+}
+
+initWidgets(){
+  const thisBooking = this;
+  thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
+  thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+  thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+  thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+}
