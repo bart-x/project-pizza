379f40726c344cd0e6d6424fc2c13f1963b2be32
Bartek K <kontener00@gmail.com>
2020-08-04 22:12:46 +0200

    update app.js file with new functionalities

--- src/js/app.js
+++ src/js/app.js
@@ -1,71 +1,127 @@
 // const { active } = require("browser-sync");
-import {select, settings} from './settings.js';
+import { select, settings, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
 
-    initMenu: function () {
-      const thisApp = this;
-      // // console.log('thisApp.data', thisApp.data);
+  initPages: function () {
+    const thisApp = this;
 
-      for (let productData in thisApp.data.products) {
-        new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages) {
+      if (page.id == ifFromHash) {
+        pageMatchingHash = page.id;
+        break;
       }
-      // const testProduct = new Product();
-      // // console.log('testProduct', testProduct);
-    },
+    }
 
-    initData: function () {
-      const thisApp = this;
+    thisApp.activatePage(idFromHash);
 
-      thisApp.data = {};
-      const url = settings.db.url + '/' + settings.db.product;
+    for (let link of navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
 
-      fetch(url)
-        .then(function (rawResponse) {
-          return rawResponse.json();
-        })
-        .then(function (parsedResponse) {
-          console.log('parsedResponse', parsedResponse);
+        /* get page id from href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
 
-          /* save parsedResponse as thisApp.data.products */
-          thisApp.data.products = parsedResponse;
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
 
-          /* execute initMenu method */
-          thisApp.initMenu();
-        });
-
-      console.log('thisApp.data', JSON.stringify(thisApp.data));
-    },
-
-    initCart: function () {
-      const thisApp = this;
-
-      const cartElem = document.querySelector(select.containerOf.cart);
-      thisApp.cart = new Cart(cartElem);
-
-      thisApp.productList = document.querySelector(select.containerOf.menu);
-
-      thisApp.productList.addEventListener('add-to-cart', function(event){
-        app.cart.add(event.detail.product);
+        /* change URL hash */
+        window.location.hash = '#/' + id;
       });
-    },
+    }
+  },
 
-    init: function () {
-      const thisApp = this;
-      // // console.log('*** App starting ***');
-      // // console.log('thisApp:', thisApp);
-      // // console.log('classNames:', classNames);
-      // // console.log('settings:', settings);
-      // // console.log('templates:', templates);
+  activatePage: function (pageId) {
+    const thisApp = this;
 
-      // // console.log('thisApp.data', thisApp.data);
+    /* add class "active" to matching pages, remove from non-matching */
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
 
-      thisApp.initData();
-      thisApp.initMenu();
-      thisApp.initCart();
-    },
-  };
+    /* add class "active" to matching links, remove from non-matching */
+    for (let links of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
 
-  app.init();
+  },
+
+  initMenu: function () {
+    const thisApp = this;
+    // // console.log('thisApp.data', thisApp.data);
+
+    for (let productData in thisApp.data.products) {
+      new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+    }
+    // const testProduct = new Product();
+    // // console.log('testProduct', testProduct);
+  },
+
+  initData: function () {
+    const thisApp = this;
+
+    thisApp.data = {};
+    const url = settings.db.url + '/' + settings.db.product;
+
+    fetch(url)
+      .then(function (rawResponse) {
+        return rawResponse.json();
+      })
+      .then(function (parsedResponse) {
+        console.log('parsedResponse', parsedResponse);
+
+        /* save parsedResponse as thisApp.data.products */
+        thisApp.data.products = parsedResponse;
+
+        /* execute initMenu method */
+        thisApp.initMenu();
+      });
+
+    console.log('thisApp.data', JSON.stringify(thisApp.data));
+  },
+
+  initCart: function () {
+    const thisApp = this;
+
+    const cartElem = document.querySelector(select.containerOf.cart);
+    thisApp.cart = new Cart(cartElem);
+
+    thisApp.productList = document.querySelector(select.containerOf.menu);
+
+    thisApp.productList.addEventListener('add-to-cart', function (event) {
+      app.cart.add(event.detail.product);
+    });
+  },
+
+  init: function () {
+    const thisApp = this;
+    // // console.log('*** App starting ***');
+    // // console.log('thisApp:', thisApp);
+    // // console.log('classNames:', classNames);
+    // // console.log('settings:', settings);
+    // // console.log('templates:', templates);
+
+    // // console.log('thisApp.data', thisApp.data);
+
+    thisApp.initPages();
+
+    thisApp.initData();
+    thisApp.initMenu();
+    thisApp.initCart();
+  },
+};
+
+app.init();
